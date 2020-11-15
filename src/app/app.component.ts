b1398fd7271f76fe05883e001ae1e5c246f671e1
akaKAIN <mkain@mail.ru>
2020-11-15 16:33:14 +1000

    Fix seo-card image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,18 @@ import {Meta, Title} from '@angular/platform-browser';
 export class AppComponent {
   constructor(private meta: Meta, private title: Title) {
     this.title.setTitle('RealMeat | Мясные изделия на заказ');
-    this.meta.addTag({name: 'application', content: 'RealMeat site'});
-    this.meta.addTag({name: 'author', content: 'KolbasAnna'});
+    this.meta.addTag({name: 'application', content: 'site:RealMeat'});
+    this.meta.addTag({name: 'author', content: 'akaKAIN'});
     this.meta.addTag({name: 'image', content: './assets/img/special/snack_meat_meta-card.webp'});
-    this.meta.addTag({name: 'description', content: 'Мета описание сайта.'});
+    this.meta.addTag({name: 'description', content: ''});
 
     this.meta.addTags([
       {property: 'og:title', content: 'Мясные вкусности на заказ.'},
       {property: 'og:url', content: 'https://real-meat.herokuapp.com/home'},
-      {property: 'og:image', content: './assets/img/special/snack_meat_meta-card.webp'},
+      {property: 'og:image', content: 'https://real-meat.herokuapp.com/assets/img/special/snack_meat_meta-card.webp'},
       {property: 'og:local', content: 'ru_RU'},
-      {property: 'og:description', content: 'Авторская работа.'},
-      {property: 'og:site_name', content: 'REAL MEAT - это индивидаульный подход.'},
+      {property: 'og:description', content: 'У нас индивидуальный подход к каждому заказу.'},
+      {property: 'og:site_name', content: 'RealMeat'},
     ]);
   }
 }
