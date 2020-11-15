15bbd13d9ac273e0e265104a3d3cb96d204942e4
akaKAIN <mkain@mail.ru>
2020-11-15 13:13:27 +1000

    Replace of meta tags integration. For tests.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTag({name: 'application', content: 'RealMeat site'});
+    this.meta.addTag({name: 'author', content: 'KolbasAnna'});
+    this.meta.addTag({name: 'image', content: 'assets/img/special/snack_meat_meta-card.webp'});
+    this.meta.addTag({name: 'description', content: 'Мета описание сайта.'});
+  }
+}
