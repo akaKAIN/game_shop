aec102e8df90c2a2be1cc9033907eb5a2ee507d1
akaKAIN <mkain@mail.ru>
2020-11-15 13:27:07 +1000

    Change absolute path in component source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   constructor(private meta: Meta) {
     this.meta.addTag({name: 'application', content: 'RealMeat site'});
     this.meta.addTag({name: 'author', content: 'KolbasAnna'});
-    this.meta.addTag({name: 'image', content: 'assets/img/special/snack_meat_meta-card.webp'});
+    this.meta.addTag({name: 'image', content: './assets/img/special/snack_meat_meta-card.webp'});
     this.meta.addTag({name: 'description', content: 'Мета описание сайта.'});
   }
 }
